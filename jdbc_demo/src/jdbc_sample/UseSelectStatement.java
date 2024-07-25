49b282ce472e4bedcdd08b34ec589443a40c502b
bzf <bzf@DESKTOP-3ENAFVQ>
2024-07-25 15:27:40 +0530

    commit java prg

--- jdbc_demo/src/jdbc_sample/UseSelectStatement.java
+++ jdbc_demo/src/jdbc_sample/UseSelectStatement.java
@@ -3,6 +3,7 @@ package jdbc_sample;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -15,12 +16,13 @@ public class UseSelectStatement {
 			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdemo","root","Mani123");
 			Statement st=con.createStatement();
 			ResultSet rs = st.executeQuery(query);
+			ResultSetMetaData md = rs.getMetaData();
+			for(int i=1;i<=5;i++) {
+			//	System.out.print(md.getColumnName(i)+" ");
+			}
+			System.out.println();
 			while(rs.next()) {
-			System.out.println("id: "+rs.getInt(1));
-			System.out.println("name: "+rs.getString(2));
-			System.out.println("country code: "+rs.getInt(3));
-			System.out.println("district: "+rs.getString(4));
-		    System.out.println("population: "+rs.getInt(5));
+			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getInt(3)+" "+rs.getString(4)+" "+rs.getInt(5));
 			System.out.println();
 			}
 		} catch (SQLException e) {
